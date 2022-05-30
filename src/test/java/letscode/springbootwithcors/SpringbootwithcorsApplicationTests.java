c05a23c360b494e5174ecc28d08330e97a21956d
Phoenix787 <shiny777@bk.ru>
2022-05-30 16:07:38 +0500

    initial

---
+++ src/test/java/letscode/springbootwithcors/SpringbootwithcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package letscode.springbootwithcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
